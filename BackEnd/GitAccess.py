7725bf75d7392d522daf1d5ff631e269f546f316
geselc <geselc@uw.edu>
2019-02-17 13:24:19 -0800

    updated GitAccess with getFileVersion

--- BackEnd/GitAccess.py
+++ BackEnd/GitAccess.py
@@ -1,14 +1,42 @@
 import os
 
-# filelist is a list of files to be committed
+# This method creates a new repo and
+def createRepo(reponame):
+    return None
+
+
+# This method returns the files in a repo to populate
+# the file/commit browser (???)
+def loadRepo(reponame):
+    return None
+
+
+# This method adds and commits a list of files
 def commit(filelist, message):
     os.system("git add " + " ".join(filelist))
     os.system("git commit -m " + "\"" + message + "\"")
-    os.system("git push")
+
+
+# This method returns the code for a specific version of a file
+# filename must be a path to the given file
+def getFileVersion(commit, filepath):
+    return os.popen("git show " + commit + ":" + filepath).read()
+
+# This method returns the name and time of every commit that
+# modified a file.
+def getFileVersionHistory(filename):
+    return None
+
+# This commit returns the name of every file that was modified in
+# a given commit
+def getCommit(commit):
+    return None
+
 
 
 def main():
-    commit(["GitAccess.py"], "Testing Commit from a Python File")
+    # commit(["GitAccess.py"], "Testing Commit from a Python File")
+    getFileVersion("27819af5d2f0bea526e1c177feb08f68563839c0", "BackEnd/GitAccess.py")
 
 
 if __name__ == '__main__':
