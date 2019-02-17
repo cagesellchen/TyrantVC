f81881b18a814c3f95f9373201e3ba131fa2d0fc
geselc <geselc@uw.edu>
2019-02-17 13:54:15 -0800

    GitAccess getFileVersionHistory

--- BackEnd/GitAccess.py
+++ BackEnd/GitAccess.py
@@ -22,10 +22,23 @@ def commit(filelist, message):
 def getFileVersion(commit, filepath):
     return os.popen("git show " + commit + ":" + filepath).read()
 
-# This method returns the name and time of every commit that
-# modified a file.
+# This method returns the name, date, and message of every commit
+# that modified a given file.
+# return data format: [[commit, date, message],[]...]
 def getFileVersionHistory(filename):
-    return None
+    outlist = os.popen("git log --follow -- " + filename).read().splitlines()
+    outlist.append("")  # last commit entry doesn't have a blank line after it
+                        # resulting in one less line than the other entries
+
+    data = []
+    for i in range(int(len(outlist)/6)):
+        commit = outlist[i*6][7:]
+        date = outlist[i*6 + 2][8:]
+        message = outlist[i*6 + 4][4:]
+        data.append([commit, date, message])
+
+    return data
+
 
 # This commit returns the name of every file that was modified in
 # a given commit
@@ -36,7 +49,8 @@ def getCommit(commit):
 
 def main():
     # commit(["GitAccess.py"], "Testing Commit from a Python File")
-    getFileVersion("27819af5d2f0bea526e1c177feb08f68563839c0", "BackEnd/GitAccess.py")
+    # getFileVersion("27819af5d2f0bea526e1c177feb08f68563839c0", "BackEnd/GitAccess.py")
+    getFileVersionHistory("GitAccess.py")
 
 
 if __name__ == '__main__':
