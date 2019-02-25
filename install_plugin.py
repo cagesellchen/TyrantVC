ffd270ae2395b6d2cfe286e6a332ff25d72e5e50
Volodymyr Loyko <volodymyr.loyko@gmail.com>
2019-02-24 18:45:17 -0800

    make plugin installable

--- install_plugin.py
+++ install_plugin.py
@@ -1,10 +1,28 @@
 from sys import platform
+import os
 if platform == "linux" or platform == "linux2":
     # linux
-    print("linux")
+    print("TyrantVC is incompatible with Linux operating system.")
 elif platform == "darwin":
     # OS X
-    print("osx")
-elif platform == "win32":
+    print("Installing TyrantVC on OSX...")
+    if(not os.path.isdir("/Users/Shared/Autodesk/maya/plug-ins")):
+        os.makedirs("/Users/Shared/Autodesk/maya/plug-ins")
+    os.chdir("/Users/Shared/Autodesk/maya/plug-ins")
+    print("Downloading TyrantVC...")
+    os.system("git clone https://github.com/cagesellchen/TyrantVC.git")
+    print("Successfully download TyrantVC...\nSetting up the files...")
+    os.system("mv TyrantVC/tyrantvc.py tyrantvc.py")
+    print("TyrantVC installed! Please restart Maya and search for \"tyrantvc\" in the Plug-In Manager.")
+    
+elif platform == "win32" or platform == "win64":
     # Windows...
-    print("win32")
+    print("Installing TyrantVC on Windows...")
+    if(not os.path.isdir("C:\\Program Files\\Autodesk\\Maya2019\\bin\\plug-ins")):
+        os.makedirs("C:\\Program Files\\Autodesk\\Maya2019\\bin\\plug-ins")
+    os.chdir("C:\\Program Files\\Autodesk\\Maya2019\\bin\\plug-ins")
+    print("Downloading TyrantVC...")
+    os.system("git clone https://github.com/cagesellchen/TyrantVC.git")
+    print("Successfully download TyrantVC...\nSetting up the files...")
+    os.system("mv TyrantVC/tyrantvc.py tyrantvc.py")
+    print("TyrantVC installed! Please restart Maya and search for \"tyrantvc\" in the Plug-In Manager.")
