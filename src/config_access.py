3a1b81463ca85d155faf5ea4445fd8e8f135c3c3
geselc <geselc@uw.edu>
2019-03-03 20:01:53 -0800

    config_access works for all OS

--- src/config_access.py
+++ src/config_access.py
@@ -3,17 +3,18 @@ import os
 # This method checks to see if a config.txt file exists in the
 # TyrantVC folder. If it doesn't, it creates one.
 def init_config():
-    os.chdir(os.path.expanduser("~/Library/Preferences/Autodesk/maya/plug-ins/TyrantVC"))
+    os.chdir(os.path.expanduser(os.path.join(os.getcwd(), os.pardir)))
     ls = os.popen("ls").read().splitlines()
 
     if "config.txt" not in ls:
-        os.system("touch config.txt")
+        os.system("touch ../config.txt")
+
 
 # This method returns a list of tuples of the data in the config.txt file.
 # Tuple[0] is the repo name
 # Tuple[1] is the repo path
 def parse_config():
-    os.chdir(os.path.expanduser("~/Library/Preferences/Autodesk/maya/plug-ins/TyrantVC"))
+    os.chdir(os.path.expanduser(os.getcwd()))
     config_txt = os.popen("cat config.txt").read().splitlines()
 
     tuple_list = []
@@ -23,11 +24,12 @@ def parse_config():
 
     return tuple_list
 
+
 # This method adds a repo and path to the config.txt file
 # repo_name is the name of the repo
 # repo_path is the path to the repo
 def add_config(repo_name, repo_path):
-    os.chdir(os.path.expanduser("~/Library/Preferences/Autodesk/maya/plug-ins/TyrantVC"))
+    os.chdir(os.path.expanduser(os.getcwd()))
     f = open("config.txt", "a")
-    f.write(str(repo_name) + " " + str(repo_path))
+    f.write(repo_name + " " + repo_path + "\n")
     f.close()
